f7a697e753743fec3f3324978e8fe7e0f2dfdb85
23mkarhapoltseva <23mkarhapoltseva@heckgrammar.co.uk>
2025-01-20 22:19:35 +0000

    Update Program.cs
    tried at home

--- HowToUseGitHubClassroom/Program.cs
+++ HowToUseGitHubClassroom/Program.cs
@@ -21,6 +21,12 @@
             // Output to the console the variable
             // Output to the console Hello World
             // Test your program and add the evidence to this repository
+
+
+            Control.WriteLine("Enter your name");
+                string name = Console.ReadLine (name);
+            Console.WriteLine(name);
+            Console.WriteLine(hello world);
         }
     }
 }
